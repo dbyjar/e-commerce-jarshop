c8d4efb8efd02cf5e22ebf386190601421a78824
dbyjar <fjralhakim@gmail.com>
2020-09-28 10:23:05 +0700

    modified list profile page

--- module/profile/list.php
+++ module/profile/list.php
@@ -1,15 +1,20 @@
-<div id="isi-profile">
-	<h3>Profilmu
-		<a class="tombol-action"
-			href="<?php echo BASE_URL."index.php?page=menu&module=profile&action=form"; ?>">Update
-		</a>
-	</h3>
-	<h4>
-		Nama : <?php echo "$nama"; ?>
-		<a class="tombol-action" href="<?php echo BASE_URL."keluar.php"; ?>">Keluar</a>	
-	</h4>
-	<p>Level : <?php echo "$level"; ?></p>
-	<p>E-mail : <?php echo "$email"; ?></p>
-	<p>Hp : <?php echo "$phone"; ?></p>
-	<p>Alamat : <?php echo "$alamat"; ?></p>
+<!-- Card -->
+<div class="containe d-flex justify-content-center">
+<div class="card mt-5" style="width: 18rem;">
+  <!-- <img class="card-img-top" src='<?= BASE_URL."vendor/img/categories/category-1.jpg" ?>' alt="Card image cap"> -->
+  <div class="card-body">
+    <h5 class="card-title">Nama : <?= "$nama"; ?></h5>
+    <p class="card-text">Level : <?= "$level"; ?></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">E-mail : <?= "$email"; ?></li>
+    <li class="list-group-item">Hp : <?= "$phone"; ?></li>
+    <li class="list-group-item">Alamat : <?= "$alamat"; ?></li>
+  </ul>
+  <div class="card-body d-flex align-items-center">
+    <a class="btn btn-primary mr-auto" href="<?= BASE_URL."index.php?page=menu&module=profile&action=form"; ?>">Update</a>
+    <a href="<?= BASE_URL."keluar.php"; ?>" class="card-link">Logout</a>
+  </div>
 </div>
+</div>
+<!-- end of Card -->
