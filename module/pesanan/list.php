358ef08e6440496f40a9d1dc8579c5fd63aa47ba
dbyjar <fjralhakim@gmail.com>
2020-09-28 12:51:58 +0700

    add if functio payment page

--- module/pesanan/list.php
+++ module/pesanan/list.php
@@ -6,38 +6,84 @@
 		$queryPesanan = mysqli_query($koneksi, "SELECT pesanan.*, user.nama FROM pesanan JOIN user ON pesanan.user_id=user.user_id WHERE pesanan.user_id='$user_id' ORDER BY pesanan.tanggal_pemesanan DESC");
 	}
 	
-	if(mysqli_num_rows($queryPesanan) == 0){
-		echo "<h3>Saat ini belum ada data pesanan</h3>";
-	}
-	else{
-	
-		echo "<table class='table-list'>
-				<tr class='baris-title'>
-					<th class='kiri'>Nomor Pesanan</th>
-					<th class='kiri'>Status</th>
-					<th class='kiri'>Nama</th>
-					<th class='kiri'>Action</th>
-				</tr>";
-		
-		$adminButton = "";
-		while($row=mysqli_fetch_assoc($queryPesanan)){
-			if($level == "superadmin"){
-				$adminButton = "<a class='tombol-action' href='".BASE_URL."index.php?page=menu&module=pesanan&action=status&pesanan_id=$row[pesanan_id]'>Update Status</a>";
-			}
-		
-			$status = $row['status'];
-			echo "<tr>
-					<td class='kiri'>$row[pesanan_id]</td>
-					<td class='kiri'>$arrayStatusPesanan[$status]</td>
-					<td class='kiri'>$row[nama]</td>
-					<td class='kiri'>
-						<a class='tombol-action' href='".BASE_URL."index.php?page=menu&module=pesanan&action=detail&pesanan_id=$row[pesanan_id]'>Detail Pesanan</a>
-						$adminButton
-					</td>
-				 </tr>";
-		}
-		
-		echo "</table>";
-	}
-	
+	if(mysqli_num_rows($queryPesanan) == 0) {
+		echo "<h3>Your list order is empty</h3>";
+	} else {
+
 ?>
+
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="breadcrumb__links">
+          <a href='<?= BASE_URL ?>'><i class="fa fa-home"></i> Home</a>
+          <span>Order</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- Checkout Section Begin -->
+<section class="checkout spad">
+  <div class="container">
+    <form action="#" class="checkout__form">
+      <div class="row">
+        <div class="col-lg">
+          <h5>Your Order</h5>
+          <div class="row">
+            
+              <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" class='text-center'>Name</th>
+                    <th scope="col" class='text-center'>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+				<?php
+					$adminButton = "";
+					while($row=mysqli_fetch_assoc($queryPesanan)){
+						if($level == "superadmin"){
+							$adminButton = "<a class='btn btn-success btn-sm ml-2' href='".BASE_URL."index.php?page=menu&module=pesanan&action=status&pesanan_id=$row[pesanan_id]'>Update</a>";
+						}
+					
+						$status = $row['status'];
+						echo "<tr>
+                            <th scope='row'>$row[pesanan_id]</th>
+                            <td>$arrayStatusPesanan[$status]</td>
+                            <td class='text-center'>$row[nama]</td>
+                            <td class='text-center'><a class='btn btn-secondary btn-sm' href='".BASE_URL."index.php?page=menu&module=pesanan&action=detail&pesanan_id=$row[pesanan_id]'>Detail</a>
+									$adminButton</td>
+                          </tr>";
+					}
+					
+					echo "</tbody>
+						</table>";
+
+?>
+
+<?php } ?>
+            
+            <div class="col-lg-12">
+              <div class="checkout__form__checkbox">
+                <!-- <label for="acc">
+                Create an acount?
+                  <input type="checkbox" id="acc">
+                  <span class="checkmark"></span>
+                </label> -->
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</section>
+<!-- Checkout Section End -->
--- module/pesanan/status.php
+++ module/pesanan/status.php
@@ -7,35 +7,50 @@
 	$status = $row['status'];
 	
 ?>
-<form action="<?php echo BASE_URL."module/pesanan/action.php?pesanan_id=$pesanan_id"; ?>" method="POST">
-	 
-	<div class="element-form">
-		<label>Pesanan Id (Faktur Id)</label>    
-		<span><input type="text" value="<?php echo $pesanan_id; ?>" name="pesanan_id" readonly="true" /></span>
-	</div>  
 
-	<div class="element-form">
-		<label>Status</label>
-		<span>
-			<select name="status">
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="breadcrumb__links">
+					<a href='<?= BASE_URL ?>'><i class="fa fa-home"></i> Home</a>
+					<a href="<?= BASE_URL."index.php?page=menu&module=pesanan&action=list" ?>">Order </a>
+					<span>Update Status</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Breadcrumb End -->
+
+<div class="container my-5">
+	<div class="row">	
+		<div class="col-md-12">
+			<form action="<?php echo BASE_URL."module/pesanan/action.php?pesanan_id=$pesanan_id"; ?>" method="POST" style="width: 21rem;">
+		<div class="form-group">
+			<label>Order ID</label>
+			<input type="text" class="form-control" value="<?php echo $pesanan_id; ?>" name="pesanan_id" readonly />
+		</div>
+		<div class="form-group">
+			<label>Status</label>
+			<select class="form-control" name="status">
 				<?php
-				
+						
 					foreach($arrayStatusPesanan AS $key => $value){
 						if($status == $key){
 							echo "<option value='$key' selected='true'>$value</option>";
-						}
-						else{
+						} else {
 							echo "<option value='$key'>$value</option>";
 						}
 					}
 				
 				?>
 			</select>
-		</span>
-	</div>	
+		</div>
+		<button type="submit" type="submit" value="Edit Status" name="button" class="btn btn-primary">Submit</button>
+	</form>
+		</div>
+	</div>
 	
-	<div class="element-form">
-		<span><input class="tombol-action" type="submit" value="Edit Status" name="button" /></span>
-	</div>	
-	
-</form>  
+</div>
